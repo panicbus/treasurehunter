cd450107509a50af688bb27c91d3bfa833b041b5
Jennie Louie <jennie.louie@gmail.com>
2013-11-10 13:54:57 -0800

    Added omniauth up to but not including callback controller

--- Treasure_Hunter/db/migrate/20131110192712_create_users.rb
+++ Treasure_Hunter/db/migrate/20131110192712_create_users.rb
@@ -3,6 +3,9 @@ class CreateUsers < ActiveRecord::Migration
     create_table :users do |t|
       t.string :username
       t.string :email
+      t.string :provider
+      t.string :uid
+      t.string :name
 
       t.timestamps
     end
