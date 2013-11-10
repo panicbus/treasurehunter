40ac263eb8dda96ee824e5e1bfa136471d27f11a
Jennie Louie <jennie.louie@gmail.com>
2013-11-10 11:58:42 -0800

    Add devise user login

--- Treasure_Hunter/config/environments/development.rb
+++ Treasure_Hunter/config/environments/development.rb
@@ -34,4 +34,7 @@ TreasureHunter::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  # For Treasure_Hunter, added Devise
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
