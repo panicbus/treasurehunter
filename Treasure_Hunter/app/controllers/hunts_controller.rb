b5617a1b731199e515a562a34d94d49f7bbb0f88
Nico Crisafulli <elementalair@gmail.com>
2013-11-11 10:47:43 -0800

    added then removed show method

--- Treasure_Hunter/app/controllers/hunts_controller.rb
+++ Treasure_Hunter/app/controllers/hunts_controller.rb
@@ -1,17 +1,17 @@
 class HuntsController < ApplicationController
 
 def index
+  @hunts = Hunt.all
 end
 
 def create
   Hunt.create(params[:hunt])
-  redirect_to hunts_path
+  # redirect_to hunts_path
 end
 
 def destroy
   Hunt.destroy(params[:id])
   redirect_to hunts_path
-
 end
 
 end
