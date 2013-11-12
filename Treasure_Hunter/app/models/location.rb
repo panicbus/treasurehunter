39df1175204a488c63c4e54f58c6bdea0bb7fa55
Jennie Louie <jennie.louie@gmail.com>
2013-11-12 09:58:06 -0800

    revised location model relationships

--- Treasure_Hunter/app/models/location.rb
+++ Treasure_Hunter/app/models/location.rb
@@ -2,8 +2,8 @@ class Location < ActiveRecord::Base
   attr_accessible :lat, :long, :loc_order
 
   has_many :clues
-  has_many :hunts
   has_many :hunt_locations
+  has_many :hunts, through: :hunt_locations
 
   validates :lat, presence: true
   validates :long, presence: true
