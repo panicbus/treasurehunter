7c7960757312d5dd492dd71f5d9983b6e7f6fd4c
Jennie Louie <jennie.louie@gmail.com>
2013-11-11 16:40:46 -0800

    Added draggable feature to markers, added way to save location to database

--- Treasure_Hunter/app/assets/javascripts/location.js
+++ Treasure_Hunter/app/assets/javascripts/location.js
@@ -1,6 +1,12 @@
-// Plots map showing current location
+
+//Define global variables
 var map;
 var currentPos;
+var latitude;
+var longitude;
+var marker;
+
+//function initialize plots map showing current location, and contains functions markCurrentLocation and codeAddress
 function initialize() {
   var mapOptions = {
     zoom: 16,
@@ -11,36 +17,68 @@ function initialize() {
   navigator.geolocation.getCurrentPosition(function(position){
     currentPos = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
      map.setCenter(currentPos);
+
   });
 
-
+//adds marker to current location
   function markCurrentLocation () {
-    console.log(currentPos);
-    var marker = new google.maps.Marker({
+    // var coordinates = document.getElementById('coordinates');
+    marker = new google.maps.Marker({
         position: currentPos,
         map: map,
+        draggable: true,
         title: 'This is your current location'
     });
+
+    console.log('current location is ' + currentPos);
+
+    // Enter lat and long into form
+    document.getElementById('location_lat').value=currentPos.nb;
+    document.getElementById('location_long').value=currentPos.ob;
+    latitude = currentPos.nb;
+    longitude = currentPos.ob;
+
+    google.maps.event.addDomListener(marker, 'dragend', function() { markerMoved(marker); } );
   };
 
+//plots map and marker showing user-entered address
   function codeAddress() {
+    var coordinates = document.getElementById('coordinates');
     var geocoder = new google.maps.Geocoder();
     var address = document.getElementById('address').value;
     geocoder.geocode( { 'address': address}, function(results, status) {
       if (status == google.maps.GeocoderStatus.OK) {
-        map.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
+        currentPos = results[0].geometry.location;
+        map.setCenter(currentPos);
+        marker = new google.maps.Marker({
             map: map,
-            position: results[0].geometry.location
+            position: currentPos,
+            draggable: true
         });
         } else {
           alert('Geocode was not successful for the following reason: ' + status);
       }
+      // Enter lat and long into form
+      document.getElementById('location_lat').value=currentPos.nb;
+      document.getElementById('location_long').value=currentPos.ob;
+      latitude = currentPos.nb;
+      console.log(latitude);
+      google.maps.event.addDomListener(marker, 'dragend', function() { markerMoved(marker); } );
     });
   }
-
+//
+  function markerMoved(movedMarker){
+    movedMarker.title = movedMarker.position;
+    console.log('new marker position is ' + movedMarker.position);
+    latitude = currentPos.nb;
+    // Enter lat and long into form
+    document.getElementById('location_lat').value=movedMarker.position.nb;
+    document.getElementById('location_long').value=movedMarker.position.ob;
+  }
+  console.log(latitude);
  google.maps.event.addDomListener(currentLocButton, 'click', markCurrentLocation);
  google.maps.event.addDomListener(searchButton, 'click', codeAddress);
+
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);
