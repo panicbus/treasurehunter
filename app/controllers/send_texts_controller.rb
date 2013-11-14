68a9078fdede4b66447bf0f91d444060a18260bd
Nico Crisafulli <elementalair@gmail.com>
2013-11-14 11:16:30 -0800

    added new text messages controller & view

--- app/controllers/send_texts_controller.rb
+++ app/controllers/send_texts_controller.rb
@@ -4,7 +4,7 @@ class SendTextsController < ApplicationController
   end
 
   def make_message
-    @user_phone_number = params[:user_phone_number]
+    @phone_number = params[:phone_number]
 
   end
 
