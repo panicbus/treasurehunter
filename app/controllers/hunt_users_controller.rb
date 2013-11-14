0e5460b3cb06d7f8a6c9c801891fbfbdabc7daee
james <jam@yahoo.com>
2013-11-14 00:05:48 -0800

    added a start button that only displays after the start time and updates the progress to 1 when clicked. also wired up the hunter screen to display current clue based on current progress, and to check if user answer is correct

--- app/controllers/hunt_users_controller.rb
+++ app/controllers/hunt_users_controller.rb
@@ -3,6 +3,7 @@ class HuntUsersController < ApplicationController
     @hunt_user = HuntUser.create(params[:hunt_user])
   end
 
+<<<<<<< HEAD
   def confirm_participation
     pending_hunter = params[:hunt_user]
     user = User.find(pending_hunter['user_id'])
@@ -23,3 +24,16 @@ class HuntUsersController < ApplicationController
     end
   end
 end
+=======
+  def update
+    @hunt_user = HuntUser.where(:hunt_id => params[:id], :user_id => current_user.id)
+
+    @hunt_user.each do |hu|
+      hu.update_attributes(progress: params[:progress])
+    end
+    respond_to do |format|
+      format
+    end
+  end
+end
+>>>>>>> added a start button that only displays after the start time and updates the progress to 1 when clicked. also wired up the hunter screen to display current clue based on current progress, and to check if user answer is correct
