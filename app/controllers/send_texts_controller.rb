eaa51f864e3485676e7d7c70da8b0e7152e5bfcf
Nico Crisafulli <elementalair@gmail.com>
2013-11-14 18:28:36 -0800

    stylings and added a the texts controller

--- app/controllers/send_texts_controller.rb
+++ app/controllers/send_texts_controller.rb
@@ -4,7 +4,7 @@ class SendTextsController < ApplicationController
   end
 
   def make_message
-    @phone_number = params[:phone_number]
+    @user_phone_number = params[:user_phone_number]
 
   end
 
