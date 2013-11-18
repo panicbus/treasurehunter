d7bf50f0031a5a05f7cc0fbe16f3295f7261199b
Leonidez Acosta <acosta.leonidez@gmail.com>
2013-11-17 23:05:35 -0800

    bug fixes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,7 +68,7 @@ TreasureHunter::Application.configure do
   config.action_mailer.smtp_settings = {
   address:              'smtp.sendgrid.net',
   port:                  587,
-  domain:               'heorku.com',
+  domain:               'heroku.com',
   user_name:             ENV['SENDGRID_USERNAME'],
   password:              ENV['SENDGRID_PASSWORD'],
   authentication:       'plain',
